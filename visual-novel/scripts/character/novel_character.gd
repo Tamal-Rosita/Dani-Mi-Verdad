ca03b314a0c52e7530a09d04df8b01d3206ea290
Jaime Hernandez <jaimevazz@gmail.com>
2025-10-20 14:32:52 -0400

    Update novel_character.gd hearing Dialogic timeline start/end events to toggle interaction call to accion

--- visual-novel/scripts/character/novel_character.gd
+++ visual-novel/scripts/character/novel_character.gd
@@ -7,6 +7,8 @@ class_name NovelCharacter extends CharacterBody3D
 @export var speed: float = 2
 @export var jump_strength: float = 3
 
+signal interaction_toggle
+
 @onready var move_action: ActionNode = $ActionContainer/Move
 @onready var jump_action: ActionNode = $ActionContainer/Jump
 
@@ -15,22 +17,36 @@ class_name NovelCharacter extends CharacterBody3D
 
 func _ready() -> void:
 	move_action.speed = speed
-	jump_action.JUMP_STRENGTH = jump_strength	
-	Dialogic.Text.speaker_updated.connect(_on_dialogic_speaker_updated) # This is working
+	jump_action.JUMP_STRENGTH = jump_strength
 	
+	Dialogic.timeline_ended.connect(_on_dialogic_timeline_ended)
+	Dialogic.timeline_started.connect(_on_dialogic_timeline_started)
+	
+	Dialogic.Text.speaker_updated.connect(_on_dialogic_speaker_updated) # This is working
 	Dialogic.Portraits.character_joined.connect(_on_dialogic_character_joined) # TODO: Use this as reference for Custom Event
+	
+func _on_dialogic_timeline_ended() -> void:
+	interaction_toggle.emit(false)
+	
+func _on_dialogic_timeline_started() -> void:
+	interaction_toggle.emit(true)
+	
 func _on_dialogic_character_joined(info: Dictionary) -> void:
 	print("Timeline character joined")
 	print(info)
 	
 func _on_dialogic_speaker_updated(new_character: DialogicCharacter) -> void:
+	if new_character == null: return
+	# print("Speaker updated: " + new_character.display_name + ":" + character.display_name)
 	if new_character == character:
-		print("Speaker updated: " + new_character.display_name)
+		print("Focusing on: " + new_character.display_name)
 		focus_character(true)
 	
 		
 func focus_character(active: bool) -> void:
 	dialogue_camera.make_current()
+	
+	## TODO: Verify following parameters
 	## Animation Focus parameter
 	animation_tree.set("parameters/conditions/Focus", active)
 	## Other parameters
@@ -38,8 +54,10 @@ func focus_character(active: bool) -> void:
 	
 func _on_interaction_area_3d_body_entered(body: Node3D) -> void:
 	if body is NovelCharacter:
-		print("Entered character interaction area")
+		# print("Entered character interaction area")
+		return
 
 func _on_interaction_area_3d_body_exited(body: Node3D) -> void:
 	if  body is NovelCharacter:
-		print("Exited character interaction area")
+		# print("Exited character interaction area")
+		return
